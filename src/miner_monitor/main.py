c1be10e2584d7cb5b9962cd5dbeed2385fc3dfe9
catomak <alextabds@gmail.com>
2022-11-29 14:30:52 +0400

    updated main

--- src/miner_monitor/main.py
+++ src/miner_monitor/main.py
@@ -27,28 +27,37 @@ def look_device(devices: dict) -> None:
 
     if not devices:
         log.info('Устройств в локальной сети не найдено')
-        return
+        return None
 
-    index_lens = {}
+    col_lens = {
+        'ip': 20,
+        'macaddr': 22,
+        'miner_type': 20,
+        'ghs5s': 11,
+        'ghsav': 11,
+        'fan_speed': 28,
+        'pcb_temp': 21,
+        'chip_temp': 21,
+        'chain_rate': 26,
+        'elapsed': 13
+    }
+
+    items = ''
     indexes = ''
-    miners = ''
+    for device, column in devices.values(), col_lens:
+        items += device.get(column).ljust(col_lens.get(column))
+        items += '\n'
+        if not indexes:
+            indexes += column.ljust(col_lens.get(column))
 
-    for device in devices.values():
-        for index, value in device.items():
-            if not (val_len := index_lens.get(index)):
-                val_len = len(str(value)) + 5
-                index_lens[index] = val_len
-                indexes += str(index).ljust(val_len)
-            miners += str(value).ljust(val_len)
-        miners += '\n'
-    look_str = "Got {len} devices:\n{indexes}\n{miners}"
-    log.info(look_str.format(len=len(devices), indexes=indexes, miners=miners))
+    print(indexes, items, sep='\n')
 
 
 def monitor():
     log.info("Searching for devices on the network...")
-    devices = ip_searcher.find_ip()
-    session_devices = get_session_devices(devices)
+    # devices = ip_searcher.find_ip()
+    # session_devices = get_session_devices(devices)
+    session_devices = {}
     look_device(session_devices)
     # MinerErrorDiag.check_session_devices(session_devices)
 
