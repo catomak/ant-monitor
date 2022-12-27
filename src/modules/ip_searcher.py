d748dc9c89e22cfea9192f051c4775ff0033204a
kotamak <alextabds@gmail.com>
2022-12-27 18:58:39 +0300

    modified ip_searcher and dependencies

--- src/modules/ip_searcher.py
+++ src/modules/ip_searcher.py
@@ -1,14 +1,8 @@
 from aioping import ping
 import asyncio
 from itertools import product
-from time import time
+from miner_monitor.config import log
 
-def get_ip_from_list():
-    pass
-
-# TODO: obtaiting base IP
-def get_ip_family():
-    pass
 
 # TODO: redo with base IP
 def ip_ranges_to_list(f_range: range, s_range: range) -> list:
@@ -17,27 +11,24 @@ def ip_ranges_to_list(f_range: range, s_range: range) -> list:
         ip_range.append("192.168.{}.{}".format(i, j))
     return ip_range
 
+
 async def ping_ip(ip: str) -> str | None:
     try:
         if await ping(ip):
             return ip
+        log.warning(f"Can't ping address: {ip}")
     except Exception as ex:
-        # print(f"{ip}: error ({ex})")
-        return None
-    
-async def ping_ip_range() -> list[str]:
-    ip_list = ip_ranges_to_list(range(88, 90), range(2, 256))
-    
-    tasks = []
-    for ip in ip_list:
-        tasks.append(asyncio.create_task(ping_ip(ip)))
-    
+        log.exception(f"Can't ping address: {ip}. Exception: {ex}")
+    return None
+
+
+async def ping_ip_range(ip_list: list[str]) -> list[str]:
+    tasks = [asyncio.create_task(ping_ip(ip)) for ip in ip_list]
     full_ip_list = await asyncio.gather(*tasks)
+    return [item for item in full_ip_list if item]
 
-    working_ip_list = [item for item in full_ip_list if item]
 
-    return working_ip_list
-
-def find_dveices_ip():
-    found_ip = asyncio.run(ping_ip_range())   
+def find_dveices_ip() -> list[str]:
+    ip_list  = ip_ranges_to_list(range(80, 90), range(2, 256))
+    found_ip = asyncio.run(ping_ip_range(ip_list))   
     return found_ip
